1d1fe66db1f255db7d4237797c232b524f4533cf
Kelle-Neal <kelle.neal@student.awesomeinc.org>
2023-03-16 19:39:10 +0000

    initial

---
+++ pseudocode.md
@@ -0,0 +1,42 @@
+# BUDGET APP
+
+## CLASSES
+
+
+
+### ATTRIBUTES
+
+- category_name
+- initial_deposit_amount
+- current_balance
+- 
+
+### INSTANCE VARIABLE
+
+- ledger
+
+### METHODS
+
+**deposit** (defaults to blank if no description entered)
+
+- deposit_amt
+- deposit_description
+
+**withdrawal** (if there is money enough to cover the amount it will return true, else false)
+
+- withdrawal_amt
+- withdrawal_desc
+- enough_for_withdrawal
+
+**get_balance** (returns the current balance of the budget category)
+
+**transfer** (transfer amt to another category)
+
+- transfer_amt
+
+**check_funds** (checks to see if a category has an entered amount available) 
+
+print(-budget category-)
+
+print(************** Category Name *************)
+
---
+++ src/budget.py
@@ -0,0 +1,37 @@
+######## CLASS ########
+class Budget:
+  def __init__(self):
+
+######## ATTRIBUTES ########
+    self.name = "name"
+    self.initial_deposit_amt = "initial_deposit_amt"
+    self.current_balance = "current_balance"
+
+######## INSTANCE VARIABLE ########    
+    self.ledger = []
+
+######## METHODS ########
+  def get_balance(self):
+    self.current_balance = "current_balance"
+
+  def check_funds(self):
+    self.enough_for_withdrawal = "enough_for_withdrawal"    
+  
+  def deposit(self):
+    self.deposit_amt = "deposit_amt" 
+    self.deposit_desc = "deposit_desc" 
+    self.deposit_to = "deposit_to"
+    
+  def withdrawal(self):
+    self.withdrawal_amt = "withdrawal_amt"
+    self.withdrawal_desc = "withdrawal_desc"
+    self.withdrawal_from = "withdrawal_from"
+
+  def transfer(self):
+    self.transfer_from = "transfer_from"
+    self.transfer_to = "transfer_to"
+    self.transfer_amt = "transfer_amt"
+
+######## OBJECTS ########    
+
+
